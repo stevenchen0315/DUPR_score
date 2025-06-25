38ab4430a6ec33aafbebdadb2615ba7b8c7d48b9
stevenchen0315 <121867936+stevenchen0315@users.noreply.github.com>
2025-06-25 13:56:17 +0800

    Update page.tsx

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,7 +1,20 @@
+import { type Metadata } from 'next'
 import PlayerPage from '@/components/PlayerPage'
 //import ScorePage from '@/components/ScorePage'
 
-export default function UserPage({ params }: { params: { username: string } }) {
+type Props = {
+  params: {
+    username: string
+  }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  return {
+    title: `DUPR Score - ${params.username}`,
+  }
+}
+
+export default function UserPage({ params }: Props) {
   return (
     <div>
       <PlayerPage username={params.username} />
