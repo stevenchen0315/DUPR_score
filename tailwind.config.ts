6625b01241254d8c29377559a5f5f90dc1a6e207
stevenchen0315 <121867936+stevenchen0315@users.noreply.github.com>
2025-07-08 15:05:18 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,17 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: 'blink 1s steps(2, start) infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' },
+        },
+      },
+    },
   },
   plugins: [],
 }
