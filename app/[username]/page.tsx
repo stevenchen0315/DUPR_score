4a6118dd4ee5a0fd4644f3b610479801e9c3dbf4
stevenchen0315 <121867936+stevenchen0315@users.noreply.github.com>
2025-11-20 15:30:43 +0800

    Update page.tsx

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -62,7 +62,7 @@ export default function UserPage({ params }: any) {
       </header>
 
       <h1 className="text-xl sm:text-2xl font-bold text-blue-600 text-center mb-4 mt-2">
-        Organizer: {webEvent || capitalizeFirstLetter(username)}
+        {webEvent || `Organizer: ${capitalizeFirstLetter(username)}`}
       </h1>
 
       <div className="flex justify-center gap-4 mb-4">
